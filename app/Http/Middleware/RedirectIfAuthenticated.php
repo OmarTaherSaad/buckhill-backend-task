0f17b49a3fb245315a6fac3fd1157d043d04be7b
Omar Taher Saad <omartahersaad@outlook.com>
2023-08-29 01:44:06 +0300

    Return JSON from guest middleware when failing

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -21,7 +21,10 @@ class RedirectIfAuthenticated
 
         foreach ($guards as $guard) {
             if (Auth::guard($guard)->check()) {
-                return redirect(RouteServiceProvider::HOME);
+                // Rejected
+                return response()->json([
+                    'message' => 'You are already logged in',
+                ], 403);
             }
         }
 

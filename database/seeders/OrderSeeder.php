7803275cb1664a19d1293c2fd9d2193f25851cf9
Omar Taher Saad <omartahersaad@outlook.com>
2023-09-02 23:11:59 +0300

    Change number of seeded orders in OrderSeeder

--- database/seeders/OrderSeeder.php
+++ database/seeders/OrderSeeder.php
@@ -13,10 +13,10 @@ class OrderSeeder extends Seeder
     public function run(): void
     {
         \App\Models\OrderStatus::factory()->count(5)->create();
-        \App\Models\Order::factory()->count(30)->create();
-        // Create for each user 30 orders
+        \App\Models\Order::factory()->count(10)->create();
+        // Create for each user 60 orders
         \App\Models\User::all()->each(function (\App\Models\User $user) {
-            \App\Models\Order::factory()->count(30)->create([
+            \App\Models\Order::factory()->count(60)->create([
                 'user_id' => $user->id,
             ]);
         });

ceae70c394b730512dd04e77d2da646a35e2cb15
Omar Taher Saad <omartahersaad@outlook.com>
2023-09-04 00:18:38 +0300

    Add Stripe API version to StripePayment model & fix its namespace

--- packages/omartahersaad/stripe-payments/src/StripePayment.php
+++ packages/omartahersaad/stripe-payments/src/StripePayment.php
@@ -1,9 +1,12 @@
 <?php
 
-namespace OmarTaherSaad\StripePayment;
+namespace OmarTaherSaad\StripePayments;
 
 class StripePayment
 {
     //Package version
     const VERSION = '1.0.0';
+
+    //Stripe API version [https://stripe.com/docs/upgrades#2023-08-16]
+    const STRIPE_API_VERSION = '2023-08-16';
 }

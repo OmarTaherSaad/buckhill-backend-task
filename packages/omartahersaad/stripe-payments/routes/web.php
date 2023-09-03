0629ca67daf24dbd0c73f073a69568cc6745388c
Omar Taher Saad <omartahersaad@outlook.com>
2023-09-04 00:55:27 +0300

    Add payment endpoint to web routes

--- packages/omartahersaad/stripe-payments/routes/web.php
+++ packages/omartahersaad/stripe-payments/routes/web.php
@@ -1,1 +1,7 @@
 <?php
+
+use Illuminate\Support\Facades\Route;
+use OmarTaherSaad\StripePayments\Http\Controllers\StripePaymentController;
+
+//Route for performing a payment
+Route::get('pay', [StripePaymentController::class, 'pay'])->name('pay');

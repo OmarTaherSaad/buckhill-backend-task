7625276d1f8899f95eb2e30895a74bde16e61e34
Omar Taher Saad <omartahersaad@outlook.com>
2023-09-05 01:23:25 +0300

    Write unit tests for currency package

---
+++ packages/omartahersaad/currentune/tests/Unit/CurrencyConverterTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace OmarTaherSaad\CurrenTune\Tests\Unit;
+
+use OmarTaherSaad\CurrenTune\CurrenTune;
+use OmarTaherSaad\CurrenTune\Tests\TestCase;
+
+class JwtTokenTestCurrencyConverterTest extends TestCase
+{
+    //Test getting rate for a currency
+    public function test_getting_rate_for_a_currency()
+    {
+        $rate = CurrenTune::getRate('USD');
+        $this->assertIsNumeric($rate);
+    }
+
+    //Test getting rate for a non existing currency
+    public function test_getting_rate_for_a_non_existing_currency()
+    {
+        $rate = CurrenTune::getRate('XYZ');
+        $this->assertFalse($rate);
+    }
+
+    //Test converting an amount from EUR to another currency
+    public function test_converting_an_amount_from_EUR_to_another_currency()
+    {
+        $amount = CurrenTune::convert(10, 'GBP');
+        $this->assertIsNumeric($amount);
+    }
+
+    //Test converting an amount from a non existing currency to another currency
+    public function test_converting_an_amount_from_a_non_existing_currency_to_another_currency()
+    {
+        $amount = CurrenTune::convert(10, 'XYZ');
+        $this->assertFalse($amount);
+    }
+}

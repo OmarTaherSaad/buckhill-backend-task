c43502d854c1d763063e887c5db9d2bd24b3d86f
Omar Taher Saad <omartahersaad@outlook.com>
2023-09-05 04:25:13 +0300

    Refactor payment API in payment package

--- packages/omartahersaad/stripe-payments/src/Http/Controllers/StripePaymentController.php
+++ packages/omartahersaad/stripe-payments/src/Http/Controllers/StripePaymentController.php
@@ -2,12 +2,12 @@
 
 namespace OmarTaherSaad\StripePayments\Http\Controllers;
 
-use OmarTaherSaad\StripePayments\Http\Requests\PerformPaymentRequest;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Validator;
-use OmarTaherSaad\StripePayments\Models\StripePaymentRequest;
 use OmarTaherSaad\StripePayments\StripePayment;
+use OmarTaherSaad\StripePayments\Models\StripePaymentRequest;
+use OmarTaherSaad\StripePayments\Http\Requests\PerformPaymentRequest;
 
 class StripePaymentController extends Controller
 {
@@ -15,35 +15,9 @@ class StripePaymentController extends Controller
     {
         // Get validated data
         $data = $request->validated();
-        // Prepare payment data
-        $paymentData = [
-            'success_url' => route('stripe-payment.callback', [
-                'status' => 'success',
-            ]) . '&session_id={CHECKOUT_SESSION_ID}',
-            'cancel_url' => route('stripe-payment.callback', [
-                'status' => 'cancel',
-            ]) . '&session_id={CHECKOUT_SESSION_ID}',
-            'line_items' => [
-                [
-                    'price_data' => [
-                        // Set currency from validated data
-                        'currency' => $data['currency'],
-                        'product_data' => [
-                            'name' => "Order #{$data['order_uuid']}",
-                        ],
-                        // Set amount from validated data and convert it to cents
-                        'unit_amount' => $data['amount'] * 100,
-                    ],
-                    'quantity' => 1,
-                ],
-            ],
-            'mode' => 'payment',
-            // Set currency from validated data
-            'client_reference_id' => $data['order_uuid'],
-        ];
         try {
             // Create a new checkout session
-            $session = StripePayment::createCheckoutSession($paymentData, $data['order_uuid']);
+            $session = StripePayment::createCheckoutSession($data, $data['order_uuid']);
             // Redirect the user to the checkout page
             return redirect($session->url);
         } catch (\Stripe\Exception\ApiErrorException $e) {
@@ -68,26 +42,24 @@ class StripePaymentController extends Controller
             'status' => 'required|string|in:success,cancel',
         ]);
         if ($validator->fails()) {
-            //
+            // No return url is specified, so we will redirect the user to a default page
+            return redirect(url("payment?") . http_build_query(['status' => $status, 'gtw' => 'stripe',], '', '&'));
         }
         $data = $validator->validated();
         //Get Request to check if it is still pending
         $stripePaymentRequest = StripePaymentRequest::firstWhere('checkout_session_id', $data['session_id']);
-        if ($stripePaymentRequest->status != 'pending') {
-            return redirect(url("payment/{$stripePaymentRequest->order_uuid}/?") . http_build_query([
-                'status' => $status,
-                'gtw' => 'stripe',
-            ], '', '&'));
+        if ($stripePaymentRequest->status == 'pending') {
+            //Get Checkout Session Status to check if the payment was really successful [Avoiding Fraud]
+            $checkoutSession = StripePayment::getCheckoutSession($data['session_id']);
+            //Check if the payment was successful
+            $isPaid = StripePayment::handlePaymentStatus($checkoutSession, $stripePaymentRequest);
+            $status = $isPaid ? 'success' : 'failure';
         }
-        //Get Checkout Session Status to check if the payment was really successful [Avoiding Fraud]
-        $checkoutSession = StripePayment::getCheckoutSession($data['session_id']);
-        //Check if the payment was successful
-        $isPaid = StripePayment::handlePaymentStatus($checkoutSession, $stripePaymentRequest);
-        $status = $isPaid ? 'success' : 'failure';
         // Redirect the user to the return page specified in the documentation
-        return redirect(url("payment/{$stripePaymentRequest->order_uuid}/?") . http_build_query([
+        $redirectUrl = url("payment/{$stripePaymentRequest->order_uuid}/?") . http_build_query([
             'status' => $status,
             'gtw' => 'stripe',
-        ], '', '&'));
+        ], '', '&');
+        return redirect($redirectUrl);
     }
 }
--- packages/omartahersaad/stripe-payments/src/StripePayment.php
+++ packages/omartahersaad/stripe-payments/src/StripePayment.php
@@ -31,15 +31,17 @@ class StripePayment
     /**
      * Create a new Checkout session
      * @param array $paymentData
-     * @param string $orderUuid
-     * @return \Stripe\Checkout\Session
      */
-    public static function createCheckoutSession(array $paymentData, $orderUuid): \Stripe\Checkout\Session
+    public static function createCheckoutSession(array $paymentData, string $orderUuid): \Stripe\Checkout\Session
     {
         // Create a new Stripe client
         $stripe = self::getClient();
         // Create a new checkout session
-        $session = $stripe->checkout->sessions->create($paymentData);
+        $session = $stripe->checkout->sessions->create(self::createSessionData(
+            $paymentData['amount'],
+            $paymentData['currency'],
+            $orderUuid
+        ));
         // Create a new Stripe payment request and save session ID & payload in it
         StripePaymentRequest::create([
             'order_uuid' => $orderUuid,
@@ -47,6 +49,36 @@ class StripePayment
             'checkout_session_id' => $session->id,
         ]);
         return $session;
+    }
+
+    public static function createSessionData($amount, $currency, $orderUuid)
+    {
+        // Prepare payment data
+        return [
+            'success_url' => route('stripe-payment.callback', [
+                'status' => 'success',
+            ]) . '&session_id={CHECKOUT_SESSION_ID}',
+            'cancel_url' => route('stripe-payment.callback', [
+                'status' => 'cancel',
+            ]) . '&session_id={CHECKOUT_SESSION_ID}',
+            'line_items' => [
+                [
+                    'price_data' => [
+                        // Set currency from validated data
+                        'currency' => $currency,
+                        'product_data' => [
+                            'name' => "Order #$orderUuid",
+                        ],
+                        // Set amount from validated data and convert it to cents
+                        'unit_amount' => $amount * 100,
+                    ],
+                    'quantity' => 1,
+                ],
+            ],
+            'mode' => 'payment',
+            // Set currency from validated data
+            'client_reference_id' => $orderUuid,
+        ];
     }
 
     //Get Checkout session by ID
@@ -80,8 +112,6 @@ class StripePayment
 
     /**
      * Handle payment status & update order status from Checkout session
-     * @param \Stripe\Checkout\Session $session
-     * @param StripePaymentRequest $stripePaymentRequest
      * @return bool Whether the payment was successful or not
      */
     public static function handlePaymentStatus(
@@ -93,21 +123,13 @@ class StripePayment
             $orderModelClass = config('stripe-payments.order_model');
             $order = $orderModelClass::firstWhere('uuid', $stripePaymentRequest->order_uuid);
 
-            $isPaid = $session && $session->payment_status == 'paid';
+            $isPaid = $session && $session->payment_status === 'paid';
             if ($isPaid) {
                 //Get Charge to get payment method details
                 $charge = self::getChargeFromCheckoutSession($session);
-                //Save the payment
-                $paymentModelClass = config('stripe-payments.payment_model');
                 //Assume that payment is whether credit card or bank transfer [For task limitations sake]
-                $paymentMethod = $charge->payment_method_details['type'] == 'card' ? 'credit_card' : 'bank_transfer';
-                $payment = $paymentModelClass::create([
-                    'type' => $paymentMethod,
-                    'details' => $charge->payment_method_details->values(),
-                ]);
-                //Attach the payment to the order
-                $order->payment()->associate($payment->id);
-                $order->save();
+                $paymentMethod = $charge->payment_method_details['type'] === 'card' ? 'credit_card' : 'bank_transfer';
+                self::savePayment($order, $charge, $paymentMethod);
                 //Order status must be handled by the user
             }
             $stripePaymentRequest->update([
@@ -126,5 +148,18 @@ class StripePayment
             ]);
             return false;
         }
+    }
+
+    public function savePayment($order, $charge, $paymentMethod)
+    {
+        //Save the payment
+        $paymentModelClass = config('stripe-payments.payment_model');
+        $payment = $paymentModelClass::create([
+            'type' => $paymentMethod,
+            'details' => $charge->payment_method_details->values(),
+        ]);
+        //Attach the payment to the order
+        $order->payment()->associate($payment->id);
+        $order->save();
     }
 }
